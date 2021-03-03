638445129197f18174b08248161c91f016b684c3
dan13ram <dan13ram@gmail.com>
2021-03-03 15:21:34 +0530

    renamed User to UserToken

--- src/mappings.ts
+++ src/mappings.ts
@@ -1,4 +1,4 @@
-import {User} from '../generated/schema';
+import {UserToken} from '../generated/schema';
 
 import {
   Transfer as SEEDTransferEvent,
@@ -12,82 +12,82 @@ import {
 import {
   createSEEDTransfer,
   createpSEEDTransfer,
-  createUser,
+  createUserToken,
   getSEEDBalance,
   getpSEEDBalance,
 } from './helpers';
 import {BigInt} from '@graphprotocol/graph-ts';
 
 export function handleSEED(event: SEEDTransferEvent): void {
-  let fromUser = User.load(event.params.from.toHexString());
-  if (fromUser == null) {
-    fromUser = createUser(event.params.from);
+  let fromUserToken = UserToken.load(event.params.from.toHexString());
+  if (fromUserToken == null) {
+    fromUserToken = createUserToken(event.params.from);
   }
-  let toUser = User.load(event.params.to.toHexString());
-  if (toUser == null) {
-    toUser = createUser(event.params.to);
+  let toUserToken = UserToken.load(event.params.to.toHexString());
+  if (toUserToken == null) {
+    toUserToken = createUserToken(event.params.to);
   }
 
   let transfer = createSEEDTransfer(event);
 
-  let seedTransfers = fromUser.seedTransfers;
+  let seedTransfers = fromUserToken.seedTransfers;
   seedTransfers.push(transfer.id);
-  fromUser.seedTransfers = seedTransfers;
+  fromUserToken.seedTransfers = seedTransfers;
 
-  seedTransfers = toUser.seedTransfers;
+  seedTransfers = toUserToken.seedTransfers;
   seedTransfers.push(transfer.id);
-  toUser.seedTransfers = seedTransfers;
+  toUserToken.seedTransfers = seedTransfers;
 
   let tokenInstance = SEED.bind(event.address);
 
   let fromBalance = getSEEDBalance(tokenInstance, event.params.from);
   if (fromBalance != null) {
-    fromUser.seedBalance = fromBalance as BigInt;
+    fromUserToken.seedBalance = fromBalance as BigInt;
   }
 
   let toBalance = getSEEDBalance(tokenInstance, event.params.to);
   if (toBalance != null) {
-    toUser.seedBalance = toBalance as BigInt;
+    toUserToken.seedBalance = toBalance as BigInt;
   }
 
   transfer.save();
-  fromUser.save();
-  toUser.save();
+  fromUserToken.save();
+  toUserToken.save();
 }
 
 export function handlepSEED(event: pSEEDTransferEvent): void {
-  let fromUser = User.load(event.params.from.toHexString());
-  if (fromUser == null) {
-    fromUser = createUser(event.params.from);
+  let fromUserToken = UserToken.load(event.params.from.toHexString());
+  if (fromUserToken == null) {
+    fromUserToken = createUserToken(event.params.from);
   }
-  let toUser = User.load(event.params.to.toHexString());
-  if (toUser == null) {
-    toUser = createUser(event.params.to);
+  let toUserToken = UserToken.load(event.params.to.toHexString());
+  if (toUserToken == null) {
+    toUserToken = createUserToken(event.params.to);
   }
 
   let transfer = createpSEEDTransfer(event);
 
-  let pSeedTransfers = fromUser.pSeedTransfers;
+  let pSeedTransfers = fromUserToken.pSeedTransfers;
   pSeedTransfers.push(transfer.id);
-  fromUser.pSeedTransfers = pSeedTransfers;
+  fromUserToken.pSeedTransfers = pSeedTransfers;
 
-  pSeedTransfers = toUser.pSeedTransfers;
+  pSeedTransfers = toUserToken.pSeedTransfers;
   pSeedTransfers.push(transfer.id);
-  toUser.pSeedTransfers = pSeedTransfers;
+  toUserToken.pSeedTransfers = pSeedTransfers;
 
   let tokenInstance = pSEED.bind(event.address);
 
   let fromBalance = getpSEEDBalance(tokenInstance, event.params.from);
   if (fromBalance != null) {
-    fromUser.pSeedBalance = fromBalance as BigInt;
+    fromUserToken.pSeedBalance = fromBalance as BigInt;
   }
 
   let toBalance = getpSEEDBalance(tokenInstance, event.params.to);
   if (toBalance != null) {
-    toUser.pSeedBalance = toBalance as BigInt;
+    toUserToken.pSeedBalance = toBalance as BigInt;
   }
 
   transfer.save();
-  fromUser.save();
-  toUser.save();
+  fromUserToken.save();
+  toUserToken.save();
 }
